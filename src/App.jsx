bd11249d903f7b6fdaba74f288e0593ba6e030b5
Tribhuwan <mathincosmos@gmail.com>
2023-04-06 22:18:41 +0530

    create protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,47 @@
-import { useState } from 'react'
-
+import { useState } from "react";
+import {
+  Routes,
+  Route,
+  BrowserRouter as Router,
+  Navigate,
+} from "react-router-dom";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Notes from "./components/Notes/Notes";
 function App() {
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+  });
+
   return (
-    <div className="App bg-red-500">
-      Hello World !
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            user.email ? (
+              <Navigate to="/notes" replace={true} />
+            ) : (
+              <Navigate to="/signup" replace="true" />
+            )
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/notes"
+          element={
+            user.email ? <Notes /> : <Navigate to="/signup" replace="true" />
+          }
+        />
+        <Route
+          path="*"
+          element={<h1 className="text-center text-3xl">404 Page Not Found</h1>}
+        />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
